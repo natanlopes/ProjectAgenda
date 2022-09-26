6ed417460660230400b658f2e886b1d44c4e313b
natanlopes <natan1918@gmail.com>
2022-09-26 09:49:13 -0300

    adicionando um condiçao mostra e deixa oculto do contato e habilitar para que possa alterar o telefone

--- contatos/admin.py
+++ contatos/admin.py
@@ -3,11 +3,12 @@ from .models import Categoria, Contato
 
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', 'mostrar')
     # list_filter = ('nome', 'sobrenome', 'email', 'categoria')
     list_display_links = ('id', 'nome', 'sobrenome')
     list_per_page = 5
     search_fields = ('nome', 'sobrenome', 'email', 'telefone')
+    list_editable = ('telefone','mostrar')
 
 # Register your models here.
 admin.site.register(Categoria)
